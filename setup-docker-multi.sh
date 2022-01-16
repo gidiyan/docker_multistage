ba83d0679d47e10d7f02403186d5ebe6ff60ca4c
Eugene Hidian <gidiyan@gmail.com>
2022-01-16 11:48:17 +0200

    updated images in docker-compose.yml and setup-docker-multi.sh

--- setup-docker-multi.sh
+++ setup-docker-multi.sh
@@ -4,4 +4,4 @@
 docker run -d --name es --net foodtrucks-net -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.3.2
 
 # start the flask app container
-docker run -d --net foodtrucks-net -p 5000:5000 --name foodtrucks-web back-end:multi.v.3
+docker run -d --net foodtrucks-net -p 5000:5000 --name foodtrucks-web gidiyan/multistage_building:v.0.0.1
